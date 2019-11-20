1ec3861caa529b21aaa6540401b802cc28a07054
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2019-11-20 12:36:33 +0200

    Update swift package declaration.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "TDBadgedCell",
+    platforms: [.iOS(.v8)],
     products: [
         .library(
             name: "TDBadgedCell",
